26e441fa623b9d18d91b7c582f0e9afbb1f6abd9
SamanW <saman_waruka@cmu.ac.th>
2020-03-01 23:44:46 +0700

    use react-images-upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ImageUploader from 'react-images-upload';
+ 
+const   App = () =>  {
+    let [pictures,setPictures] = useState([]);
+    console.log("REnder Component",pictures )
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const onDrop = (picture) =>  {
+      console.log( ' OnDrop ==> ', pictures);
+      setPictures(picture);
+      console.log( ' OnDrop setPictures ==> ', pictures);
+
+    }
+
+    const removeImage = (index) => {
+      console.log(" Delete ", index);
+      console.log(" pictures ", pictures);
+      setPictures(pictures.filter((value, i)=> { return i !== index;}));
+    }
+ 
+
+        return (
+          <>
+          {pictures.length < 6 ?
+          (<ImageUploader
+                withIcon={true}
+                withPreview= {true}
+                withLabel= {true}
+                label= { 'Max file size: 5mb, accepted: jpg | gif | png'}
+                accept = "image/png, image/jpeg, , image/png"
+                buttonText='Choose images'
+                onChange={onDrop}
+                imgExtension={['.jpg', '.gif', '.png']}
+                maxFileSize={5242880}
+            />): ('Only 6 images can be uploaded at a time') }
+            {/* {console.log(pictures ) } */}
+            {  pictures.map( (picture, i) => <div  key={`container${i}`} id = {`container${i}`} ><button key={`delete${i}`} id={`delete${i}`} onClick={()=> removeImage(i) }>X</button><img src={ URL.createObjectURL(picture)} key={i} id={i} alt={picture.name} height="400" width="400"/></div>)}
+            
+          </>
+
+        );
+
 }
 
 export default App;
